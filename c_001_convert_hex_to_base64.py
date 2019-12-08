f64f842ea877d643ff198df07750a8e7710e381a
sgs <g.saviano@gmail.com>
2019-12-08 21:05:32 +0100

    challenge 1

--- c_001_convert_hex_to_base64.py
+++ c_001_convert_hex_to_base64.py
@@ -13,30 +13,24 @@ So go ahead and make that happen. You'll need to use this code for the rest of t
 
 Cryptopals Rule
 Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
-
 """
 
-import binascii
 import base64
-import sys
+import binascii
 
 
-def test_hex_to_base64():
-    given_hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-    given_b64 = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
-    assert (hex_to_base64(given_hex) == given_b64)
+def hex_to_base64(hex_string):
+    """
+    >>> given_hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+    >>> given_b64 = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
+    >>> assert(hex_to_base64(given_hex) == given_b64)
+    """
 
-def hex_to_base64(hex_):
-    unhexlified_bytes = binascii.a2b_hex(hex_)
-
-    # print(unhexlified_bytes.decode('ascii'))
-    # I'm killing your brain like a poisonous mushroom
-
-    b64_bytes = base64.b64encode(unhexlified_bytes)
-    return b64_bytes.decode('ascii')
+    _bytes = binascii.a2b_hex(hex_string)
+    return str(base64.b64encode(_bytes))
 
 
 if __name__ == '__main__':
-    test_hex_to_base64()
-    print(hex_to_base64(sys.argv[1]))
+    import doctest
+    doctest.testmod()
